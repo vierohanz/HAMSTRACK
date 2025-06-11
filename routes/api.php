747f9f216242dc17d48faaada06d2cae413e7e6a
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-06-11 14:31:32 +0700

    last

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/allCollect', [CollectController::class, 'allCollect'])->name('allCollect');
 Route::get('/latestCollect', [CollectController::class, 'latestCollect'])->name('latestCollect');
 
-//collect table
+//artifficial table
 Route::get('/allTable', [ArtificialController::class, 'allTable'])->name('allTable');
 Route::get('/halfTable', [ArtificialController::class, 'halfTable'])->name('halfTable');
 Route::post('/postTable', [ArtificialController::class, 'postTable'])->name('postTable');
